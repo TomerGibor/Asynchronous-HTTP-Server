6122a75838bfc680d52fa3105e40eb4b471608a1
Tomer Gibor <46401832+TomerGibor@users.noreply.github.com>
2020-05-06 10:26:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import asyncio
 
 from HTTP_server import TcpServerProtocol
 
-IP = "0.0.0.0"
+IP = '0.0.0.0'
 PORT = 8080
 
 
@@ -14,9 +14,9 @@ async def main() -> None:
     server = await loop.create_server(lambda: TcpServerProtocol(), IP, PORT)
 
     async with server:
-        print(f"Server listening on {IP}:{PORT}")
+        print(f'Server listening on {IP}:{PORT}')
         await server.serve_forever()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     asyncio.run(main())
