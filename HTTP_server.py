81d1a9750770177f67305b5da758695afe07fb58
Tomer Gibor <46401832+TomerGibor@users.noreply.github.com>
2020-05-21 23:07:26 +0300

    Update HTTP_server.py

--- HTTP_server.py
+++ HTTP_server.py
@@ -1,8 +1,8 @@
 """
 Asynchronous HTTP Server.
 """
-import asyncio
 
+import asyncio
 from request_parser import parse_request
 from request_handler import handle_request
 
@@ -10,21 +10,25 @@ from request_handler import handle_request
 class TcpServerProtocol(asyncio.Protocol):
     """
     Asynchronous TCP server.
+
+    Attributes:
+        transport (asyncio.Transport):  Transport that represents the
+         connection with client.
+        peername (Tuple[str, int]): IPv4 Address of the client and port.
     """
 
     def __init__(self):
-        """
-        :ivar Transport transport:  Transport that represents the connection with client.
-        :ivar Tuple[str, int] peername: IPv4 Address of the client and port.
-        """
         self.transport = None
         self.peername = None
 
     def connection_made(self, transport: asyncio.Transport) -> None:
         """
         Handle new connection with client.
-        :param transport: The async object representing the connection with client.
+
+        Args:
+            transport: The async object representing the connection with client.
         """
+
         self.peername = transport.get_extra_info('peername')
         self.transport = transport
         print(f'Connection from {self.peername}')
@@ -33,16 +37,19 @@ class TcpServerProtocol(asyncio.Protocol):
         """
         Occurs when connection lost with client.
 
-        :param transport: The async object representing the connection with client.
+        Args:
+             transport: The async object representing the connection with client.
         """
+
         print(f'The client {self.peername} has closed the connection')
 
     def data_received(self, data: bytes) -> None:
         """
         Handle data received from client and send response.
-        :param data: Raw data received from the client.
+
+        Args:
+            data: Raw data received from the client.
         """
+
         request_line_header, args = parse_request(data)
         handle_request(request_line_header, self.transport, args)
-        # response = responder.handle_request(request_line_header, args)
-        # self.transport.write(response)
