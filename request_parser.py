670d8fed27e2f6bf4d46c818d0c544b016a030af
Tomer Gibor <46401832+TomerGibor@users.noreply.github.com>
2020-05-21 23:09:27 +0300

    Update request_parser.py

--- request_parser.py
+++ request_parser.py
@@ -3,26 +3,32 @@ This module is used to parse HTTP requests sent by the HTTP client.
 """
 
 from dataclasses import dataclass
+from typing import Tuple
 
 ROOT_DIR = "source_files/"
 
 
 @dataclass
 class RequestLineHeader:
-    """ Model describing first request line. """
+    """ Model describing first request line."""
     method: str
     path: str
     http_version: str
 
 
-def parse_request(request: bytes) -> RequestLineHeader:
+def parse_request(request: bytes) -> Tuple[RequestLineHeader, str]:
     """
     Parse raw data request sent by client.
 
-    :param request: Data sent by client.
-    :return: Parsed request line header and additional arguments.
+    Args:
+        request: Data sent by client.
+
+    Returns:
+        Parsed request line header and additional arguments.
     """
+
     request = request.decode('ascii')
+    print(request)
     split_request = request.split('\r\n')
     method, path, http_version = split_request[0].split(' ')
     path = ROOT_DIR + ('index.html' if path == '/' else path[1:])
